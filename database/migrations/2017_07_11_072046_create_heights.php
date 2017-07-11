60f3c4eac405d620f7f3b12b52a6229bf489eb7c
Jesús María Luis Gil <jesusmlg@gmail.com>
2017-07-11 11:12:51 +0200

    Added Baby Height, model,controller and migration

--- database/migrations/2017_07_11_072046_create_heights.php
+++ database/migrations/2017_07_11_072046_create_heights.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Heights extends Migration
+class CreateHeights extends Migration
 {
     /**
      * Run the migrations.
